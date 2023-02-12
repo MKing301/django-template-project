ec81f7a329f4376db4d7e66b8f7087b22ff747a2
MKING301 <33675411+MKing301@users.noreply.github.com>
2023-02-11 22:04:18 -0500

    Add more models

--- core/admin.py
+++ core/admin.py
@@ -1,11 +1,23 @@
 from django.contrib import admin
-from .models import User, Contact, Org, Ministry, MinistryRole, MinistryTeamMember
+from .models import (
+    User,
+    UserDetail,
+    Usa_State,
+    City,
+    Contact,
+    Org,
+    Ministry,
+    MinistryRole,
+    MinistryTeamMember
+    )
 
 
 admin.site.register(User)
+admin.site.register(UserDetail)
+admin.site.register(Usa_State)
+admin.site.register(City)
 admin.site.register(Contact)
 admin.site.register(Org)
 admin.site.register(Ministry)
 admin.site.register(MinistryRole)
 admin.site.register(MinistryTeamMember)
-
--- core/models.py
+++ core/models.py
@@ -21,6 +21,9 @@ class User(AbstractUser):
     class Meta:
         verbose_name_plural = "Users"
 
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
 
 class Usa_State(models.Model):
     name = models.CharField(max_length=75)
@@ -39,6 +42,9 @@ class City(models.Model):
         on_delete=models.CASCADE
         )
     name = models.CharField(max_length=75)
+
+    class Meta:
+        verbose_name_plural = "Cities"
 
     def __str__(self):
         return self.name
@@ -62,37 +68,40 @@ class Contact(models.Model):
 
 class Org(models.Model):
     name = models.CharField(max_length=150)
-    org_city = models.CharField(max_length=100)
-    org_state = models.CharField(max_length=10)
+    org_city = models.ForeignKey(
+        City,
+        verbose_name="Cities",
+        on_delete=models.CASCADE
+        )
+    org_state = models.ForeignKey(
+        Usa_State,
+        verbose_name="Usa_States",
+        on_delete=models.CASCADE
+        )
 
     class Meta:
         verbose_name_plural = "Orgs"
 
     def __str__(self):
-        return self.name + '-' + self.org_city + '-' + org_state
+        return self.name + '-' + self.org_city.name + '-' + self.org_state.name
 
 
 class Ministry(models.Model):
     name = models.CharField(max_length=150)
-    org =  models.ForeignKey(
+    org = models.ForeignKey(
         Org,
         verbose_name="Orgs",
         on_delete=models.CASCADE)
-
 
     class Meta:
         verbose_name_plural = "Ministries"
 
     def __str__(self):
-        return self.name + '_' + self.org
+        return self.name + '_' + self.org.name
 
 
 class MinistryRole(models.Model):
     name = models.CharField(max_length=150)
-    ministry_department = models.ForeignKey(
-        Ministry,
-        verbose_name="Ministries",
-        on_delete=models.CASCADE)
 
     class Meta:
         verbose_name_plural = "MinistryRoles"
@@ -114,3 +123,83 @@ class MinistryTeamMember(models.Model):
         MinistryRole,
         verbose_name="MinistryRoles",
         on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "MinistryTeamMembers"
+
+    def __str__(self):
+        return self.team_member.first_name + ' ' + self.team_member.last_name + ' ' + self.team_role.name + ' ' + self.ministry_department.name
+
+
+class UserDetail(models.Model):
+    class Gender(models.TextChoices):
+        FEMALE = ('FEMALE', 'Female')
+        MALE = ('MALE', 'Male')
+
+    class Marital_Status(models.TextChoices):
+        MARRIED = ('MARRIED', 'Married')
+        SINGLE = ('SINGLE', 'Single')
+        DIVORCED = ('DIVORCED', 'Divorces')
+        WIDOWED = ('WIDOWED', 'Widowed')
+
+    class Tier(models.TextChoices):
+        ONE = ('ONE', '1')
+        TWO = ('TWO', '2')
+        THREE = ('THREE', '3')
+
+    gender = models.CharField(
+        name='Gender',
+        max_length=10,
+        choices=Gender.choices,
+        null=True,
+        blank=True
+    )
+
+    marital_status = models.CharField(
+        name='Marital Status',
+        max_length=10,
+        choices=Marital_Status.choices,
+        null=True,
+        blank=True
+    )
+
+    tier = models.CharField(
+        name='Tier',
+        max_length=10,
+        choices=Tier.choices,
+        null=True,
+        blank=True
+    )
+
+    usr = models.ForeignKey(
+        User,
+        verbose_name="Users",
+        on_delete=models.CASCADE)
+    image_file = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100, null=False, default='default.jpg')
+    street_number = models.IntegerField()
+    street_name = models.CharField(max_length=50)
+    usr_city = models.ForeignKey(
+        City,
+        verbose_name="Cities",
+        on_delete=models.CASCADE
+        )
+    usr_state = models.ForeignKey(
+        Usa_State,
+        verbose_name="Usa_States",
+        on_delete=models.CASCADE
+        )
+    postal_code = models.IntegerField()
+    birthdate = models.DateField(null=True, blank=True)
+    assigned_elder = models.ForeignKey(
+        MinistryTeamMember,
+        verbose_name="MinistryTeamMembers",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True)
+    board_member = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural = "UserDetails"
+
+    def __str__(self):
+        return self.usr.first_name + ' ' + self.usr.last_name
