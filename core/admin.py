85494de09c294cf2e675c163b7b428dd54b7dbad
MKING301 <33675411+MKing301@users.noreply.github.com>
2023-02-11 20:16:39 -0500

    Add more models

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Contact, Org, Ministry, MinistryRole, MinistryTeamMember
 
 
 admin.site.register(User)
+admin.site.register(Contact)
+admin.site.register(Org)
+admin.site.register(Ministry)
+admin.site.register(MinistryRole)
+admin.site.register(MinistryTeamMember)
+
--- core/models.py
+++ core/models.py
@@ -18,6 +18,31 @@ class User(AbstractUser):
         blank=True
     )
 
+    class Meta:
+        verbose_name_plural = "Users"
+
+
+class Usa_State(models.Model):
+    name = models.CharField(max_length=75)
+
+    class Meta:
+        verbose_name_plural = "Usa_States"
+
+    def __str__(self):
+        return self.name
+
+
+class City(models.Model):
+    selected_state = models.ForeignKey(
+        Usa_State,
+        verbose_name="Usa_States",
+        on_delete=models.CASCADE
+        )
+    name = models.CharField(max_length=75)
+
+    def __str__(self):
+        return self.name
+
 
 class Contact(models.Model):
     fullname = models.CharField(max_length=75)
@@ -33,3 +58,59 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.fullname
+
+
+class Org(models.Model):
+    name = models.CharField(max_length=150)
+    org_city = models.CharField(max_length=100)
+    org_state = models.CharField(max_length=10)
+
+    class Meta:
+        verbose_name_plural = "Orgs"
+
+    def __str__(self):
+        return self.name + '-' + self.org_city + '-' + org_state
+
+
+class Ministry(models.Model):
+    name = models.CharField(max_length=150)
+    org =  models.ForeignKey(
+        Org,
+        verbose_name="Orgs",
+        on_delete=models.CASCADE)
+
+
+    class Meta:
+        verbose_name_plural = "Ministries"
+
+    def __str__(self):
+        return self.name + '_' + self.org
+
+
+class MinistryRole(models.Model):
+    name = models.CharField(max_length=150)
+    ministry_department = models.ForeignKey(
+        Ministry,
+        verbose_name="Ministries",
+        on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "MinistryRoles"
+
+    def __str__(self):
+        return self.name
+
+
+class MinistryTeamMember(models.Model):
+    team_member = models.ForeignKey(
+        User,
+        verbose_name="Users",
+        on_delete=models.CASCADE)
+    ministry_department = models.ForeignKey(
+        Ministry,
+        verbose_name="Ministries",
+        on_delete=models.CASCADE)
+    team_role = models.ForeignKey(
+        MinistryRole,
+        verbose_name="MinistryRoles",
+        on_delete=models.CASCADE)
