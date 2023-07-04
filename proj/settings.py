efa022085f71bd9f4b8dbff172bf218775a87f52
MKING301 <33675411+MKing301@users.noreply.github.com>
2023-07-04 15:52:00 -0400

    Add env var for secret key; set debug false

--- proj/settings.py
+++ proj/settings.py
@@ -16,10 +16,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wbui6$*i-r9i2k34p$w_l9wrtybl1l5dus)z8gz6d)s@3xfp#g'
+SECRET_KEY = os.environ.get('SECRET_KEY_TEMPLATE')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['192.168.1.77']
 
