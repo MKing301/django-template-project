e6aef61c370893daaff76591041144eae50103f0
MKING301 <33675411+MKing301@users.noreply.github.com>
2024-09-08 08:23:36 -0400

    Update allowed hosts

--- proj/settings.py
+++ proj/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY_TEMPLATE')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.77']
+ALLOWED_HOSTS = ['192.168.1.77', '127.0.0.1']
 
 
 # Application definition
